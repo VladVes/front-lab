b9403e2b5ecc4a0ca5ccfed4a1ab1aac852c2cdd
VladVes <veselov.workflow@gmail.com>
2022-02-09 14:59:41 +0700

    update

--- nextjs/pages/posts/categories/[id].js
+++ nextjs/pages/posts/categories/[id].js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Layout from "../../components/layout";
-import utilStyles from "../../styles/utils.module.css";
-import { getAllPostIds, getPostData } from "../../lib/posts";
-import Date from "../../components/Date";
+import Layout from "../../../components/layout";
+import utilStyles from "../../../styles/utils.module.css";
+import { getAllPostIds, getPostData } from "../../../lib/posts";
+import Date from "../../../components/Date";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
